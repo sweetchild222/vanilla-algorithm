82576fd32eb06e2a04158ffc1813fed3c0d5683f
sweetchild222 <sweetchild22.ik@gmail.com>
2021-01-12 16:56:46 +0900

    geg

--- deep/dnn.py
+++ deep/dnn.py
@@ -8,50 +8,6 @@ import datetime as dt
 from functools import partial
 
 
-def encodeOneHot(y):
-
-    unique = np.unique(y, return_counts=False)
-    oneHotMap = {key : index for index, key in enumerate(unique)}
-
-    classes = len(oneHotMap)
-    oneHotEncode = [np.eye(classes)[oneHotMap[i]].reshape(classes, 1) for i in y]
-
-    return np.array(oneHotEncode)
-
-
-def loadTestDataSet(feature_max):
-
-    test_x = []
-
-    height = feature_max[0]
-    width = feature_max[1]
-
-    for h in range(height):
-        for w in range(width):
-            feature1 = float(w)
-            feature2 = float(h)
-            test_x.append([feature1, feature2])
-
-    return np.array(test_x)
-
-
-def loadDataSet():
-
-    train_x, train_y, feature_max = extractData()
-
-    train_y = encodeOneHot(train_y)
-
-    test_x = loadTestDataSet(feature_max)
-
-    all_x = np.vstack((train_x, test_x))
-    all_x -= np.mean(all_x)
-    all_x /= np.std(all_x)
-
-    train_x = all_x[0:len(train_x)]
-    test_x = all_x[-len(test_x):]
-
-    return train_x, train_y, test_x, feature_max
-
 
 def drawPredicts(predicts, feature_max):
 
