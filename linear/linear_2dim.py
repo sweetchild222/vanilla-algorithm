e167161bce5891c099eded9881139be6a8c4a28d
sweetchild222 <sweetchild22.ik@gmail.com>
2021-02-15 16:49:43 +0900

    'gg'

--- linear/linear_2dim.py
+++ linear/linear_2dim.py
@@ -1,15 +1,13 @@
 import numpy as np
 
-def LinearRegression(X, Y, iteration):
+def LinearRegression(X, T, iteration):
 
-    weight = np.zeros((1, len(X.T)))
-    bias = np.zeros((1, 1))
-    Y = Y.reshape(-1,1)
-
+    weight = np.zeros(X.shape[-1])
+    bias = np.zeros(1)
     learning_rate = 1e-4
 
     for i in range(iteration):
-        error = prediction(weight, bias, X) - Y
+        error = prediction(weight, bias, X) - T
 
         weight_delta, bias_delta = gradient(X, error, learning_rate)
         weight -= weight_delta
@@ -17,23 +15,25 @@ def LinearRegression(X, Y, iteration):
 
     return weight, bias
 
+
 def prediction(weight, bias, X):
 
-    return np.dot(X, weight.T) + bias
+    return np.dot(X, weight) + bias
+
 
 def gradient(X, error, learning_rate):
 
-    weight_delta = (learning_rate * (1 / len(error)) * (np.dot(X.T, error)))
+    weight_delta = (learning_rate * (1 / len(error)) * (np.dot(error, X)))
     bias_delta = (learning_rate * 100 * len(error) * np.sum(error))
 
-    return weight_delta.T, bias_delta
+    return weight_delta, bias_delta
 
 
 X = np.array([[5.0, 50], [6.0, 20], [10.0, 30], [7.0, 40], [8.0, 20], [12.0, 60]])
-Y = np.array([13.0, 15.5, 22.5, 17.0, 20.0, 26.5])
+T = np.array([13.0, 15.5, 22.5, 17.0, 20.0, 26.5])
 iteration = 100000
 
-weight, bias  = LinearRegression(X, Y, iteration)
+weight, bias  = LinearRegression(X, T, iteration)
 
 print('weight  : ', weight)
 print('bias  : ', bias)
