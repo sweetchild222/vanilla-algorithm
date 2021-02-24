8ebd1fd0cee3c8e1fdccc0eb01c9cf7ce9c9ea34
sweetchild222 <sweetchild22.ik@gmail.com>
2021-02-24 09:54:04 +0900

    'update'

--- linear/linear_2dim.py
+++ linear/linear_2dim.py
@@ -1,13 +1,12 @@
 import numpy as np
 
 
-def LinearRegression(X, T, iteration):
+def LinearRegression(X, T, learning_rate, iteration):
 
     inputNodes = X.shape[-1]
     outputNodes = T.shape[-1]
     weight = np.zeros((inputNodes, outputNodes))
     bias = np.zeros(outputNodes)
-    learning_rate = 1e-4
 
     for i in range(iteration):
         error = prediction(weight, bias, X) - T
@@ -38,7 +37,7 @@ def gradient(X, error, learning_rate):
 X = np.array([[5.0, 50], [6.0, 20], [10.0, 30], [7.0, 40], [8.0, 20], [12.0, 60]])
 T = np.array([[13.0], [15.5], [22.5], [17.0], [20.0], [26.5]])
 
-weight, bias  = LinearRegression(X, T, iteration = 100000)
+weight, bias  = LinearRegression(X, T, learning_rate = 1e-4, iteration = 100000)
 
 print('weight  : ', weight)
 print('bias  : ', bias)
