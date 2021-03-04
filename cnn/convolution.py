5660f6da7792666b4515dc6795d0516be75356e7
sweetchild222 <sweetchild22.ik@gmail.com>
2021-03-04 09:55:40 +0900

    clean_code

--- cnn/convolution.py
+++ cnn/convolution.py
@@ -7,34 +7,40 @@ def activation_forward(x):
 
     return 1 / (1 + np.exp(-x))
 
-def padding(x):
 
-    pad_size = 1
-    return np.pad(x, ((0, 0), (pad_size, pad_size), (pad_size, pad_size)), 'constant', constant_values=0)
+def padding(input, weight_shape):
+
+    (weight_height, weight_width) = weight_shape
+
+    pad_height = weight_height // 2
+    pad_width = weight_width // 2
+
+    return np.pad(input, ((0, 0), (pad_height, pad_height), (pad_width, pad_width)), 'constant', constant_values=0)
+
 
 def convolution_forward(input, weight, bias):
 
-    input = padding(input)
+    input = padding(input, weight.shape)
 
     (batches, input_height, input_width) = input.shape
-    (kernel_height, kernel_width) = weight.shape
+    (weight_height, weight_width) = weight.shape
     (stride_y, stride_x) = (1, 1)
 
-    numerator_height = input_height - kernel_height
-    numerator_width = input_width - kernel_width
+    numerator_height = input_height - weight_height
+    numerator_width = input_width - weight_width
 
-    calc_shape = ((numerator_height // stride_y) + 1, (numerator_width // stride_x) + 1)
+    output_shape = ((numerator_height // stride_y) + 1, (numerator_width // stride_x) + 1)
     #a = np.array([[[1,1,1], [1,1,1], [1,1,1]], [[2,2,2], [2,2,2], [2,2,2]], [[3,3,3], [3,3,3], [3,3,3]], [[4,4,4], [4,4,4], [4,4,4]]])
     #b = np.array([[2,2,2], [2,2,2], [2,2,2]])
 
-    output = np.zeros((batches, ) + calc_shape)
+    output = np.zeros((batches, ) + output_shape)
 
     input_y = out_y = 0
-    while (input_y + kernel_height) <= input_height:
+    while (input_y + weight_height) <= input_height:
         input_x = out_x = 0
-        while (input_x + kernel_width) <= input_width:
+        while (input_x + weight_width) <= input_width:
 
-            i = input[:,input_y:input_y + kernel_height, input_x:input_x + kernel_width]
+            i = input[:,input_y:input_y + weight_height, input_x:input_x + weight_width]
 
             iw = i * weight
 
@@ -59,10 +65,10 @@ def activation_backward(input, error):
 
 def convolution_backward(input, error, weight, bias):
 
-    input = padding(input)
+    input = padding(input, weight.shape)
 
+    (weight_height, weight_width) = weight.shape
     (batches, input_height, input_width) = input.shape
-    (kernel_height, kernel_width) = weight.shape
     (stride_y, stride_x) = (1, 1)
 
     back_layer_error = np.zeros(input.shape)
@@ -72,18 +78,18 @@ def convolution_backward(input, error, weight, bias):
     b_delta = np.zeros((batches, ) + bias.shape)
 
     input_y = out_y = 0
-    while (input_y + kernel_height) <= input_height:
+    while (input_y + weight_height) <= input_height:
         input_x = out_x = 0
-        while (input_x + kernel_width) <= input_width:
+        while (input_x + weight_width) <= input_width:
             err = (error[:, out_y, out_x])[:, np.newaxis, np.newaxis]
-            i = input[:, input_y:input_y + kernel_height, input_x:input_x + kernel_width]
+            i = input[:, input_y:input_y + weight_height, input_x:input_x + weight_width]
 
             w_delta += (err * i)
             b_delta += err.reshape((batches, ) + bias.shape)
 
             bw_err = batch_weight * err
             #shallow copy
-            bl_err = back_layer_error[:, input_y:input_y + kernel_height, input_x:input_x + kernel_width]
+            bl_err = back_layer_error[:, input_y:input_y + weight_height, input_x:input_x + weight_width]
             bl_err += bw_err
 
             input_x += stride_x
