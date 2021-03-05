05ae2a3a7325ee815d292f398a92647250da33a7
sweetchild222 <sweetchild22.ik@gmail.com>
2021-03-05 17:31:54 +0900

    'layer_iterate'

--- deep_learning/emotion.py
+++ deep_learning/emotion.py
@@ -5,6 +5,7 @@ import core.convolution as conv
 import core.pooling as pool
 import core.sigmoid as sigmoid
 
+
 def print_summary(epoch, iterate, mse, predict):
 
     if ((epoch + 1) % 10) == 0:
@@ -23,38 +24,34 @@ def shuffle(x, target):
     return x[shuffle_indices], target[shuffle_indices]
 
 
-def train(x, target, convol_size, convol_stride, pool_size, pool_stride, learning_rate, iterate):
+def train(x, target, convol_size, convol_stride, pool_size, pool_stride, layer_count, learning_rate, iterate):
 
-    c1_weight = np.random.normal(size=convol_size)
-    c1_bias = np.zeros((1))
-
-    c2_weight = np.random.normal(size=convol_size)
-    c2_bias = np.zeros((1))
-
-    c3_weight = np.random.normal(size=convol_size)
-    c3_bias = np.zeros((1))
+    weight_list = [np.random.normal(size=convol_size).copy()] * layer_count
+    bias_list = [np.zeros((1)).copy()] * layer_count
 
     for i in range(iterate):
 
         x, target = shuffle(x, target)
 
-        c1 = conv.forward(x, c1_weight, c1_bias, convol_stride)
+        input = x
 
-        s1 = sigmoid.forward(c1)
+        input_conv_list = []
+        input_sigmoid_list = []
+        input_pool_list = []
 
-        p1 = pool.forward(s1, pool_size, pool_stride)
+        for l in range(layer_count):
+            input_conv_list.append(input)
+            c = conv.forward(input, weight_list[l], bias_list[l], convol_stride)
 
-        c2 = conv.forward(p1, c2_weight, c2_bias, convol_stride)
+            input_sigmoid_list.append(c)
+            s = sigmoid.forward(c)
 
-        s2 = sigmoid.forward(c2)
+            input_pool_list.append(s)
+            p = pool.forward(s, pool_size, pool_stride)
 
-        p2 = pool.forward(s2, pool_size, pool_stride)
+            input = p
 
-        c3 = conv.forward(p2, c3_weight, c3_bias, convol_stride)
-
-        s3 = sigmoid.forward(c3)
-
-        predict = pool.forward(s3, pool_size, pool_stride)
+        predict = p
 
         error = (predict - target)
 
@@ -62,35 +59,24 @@ def train(x, target, convol_size, convol_stride, pool_size, pool_stride, learnin
 
         print_summary(i, iterate, mse, predict)
 
+        for l in reversed(range(layer_count)):
 
-        error = pool.backward(s3, error, pool_size, pool_stride)
+            input_pool = input_pool_list[l]
+            error = pool.backward(input_pool, error, pool_size, pool_stride)
 
-        error = sigmoid.backward(c3, error)
+            input_sigmoid = input_sigmoid_list[l]
+            error = sigmoid.backward(input_sigmoid, error)
 
-        error, weight_delta, bias_delta = conv.backward(p2, error, c3_weight, c3_bias, convol_stride)
+            weight = weight_list[l]
+            bias = bias_list[l]
 
-        c3_weight -= (learning_rate * weight_delta)
-        c3_bias -= (learning_rate * bias_delta)
+            input_conv = input_conv_list[l]
+            error, weight_delta, bias_delta = conv.backward(input_conv, error, weight, bias, convol_stride)
 
-        error = pool.backward(s2, error, pool_size, pool_stride)
+            weight -= (learning_rate * weight_delta)
+            bias -= (learning_rate * bias_delta)
 
-        error = sigmoid.backward(c2, error)
-
-        error, weight_delta, bias_delta = conv.backward(p1, error, c2_weight, c2_bias, convol_stride)
-
-        c2_weight -= (learning_rate * weight_delta)
-        c2_bias -= (learning_rate * bias_delta)
-
-        error = pool.backward(s1, error, pool_size, pool_stride)
-
-        error = sigmoid.backward(c1, error)
-
-        error, weight_delta, bias_delta = conv.backward(x, error, c1_weight, c1_bias, convol_stride)
-
-        c1_weight -= (learning_rate * weight_delta)
-        c1_bias -= (learning_rate * bias_delta)
-
-    return c1_weight, c1_bias, c2_weight, c2_bias, c3_weight, c3_bias
+    return weight_list, bias_list
 
 
 convol_size = (3, 3)
@@ -100,29 +86,25 @@ pool_stride = pool_size
 
 train_x, train_t, test_x, test_t = loadDataSet('cnn/image/emotion/train', 'cnn/image/emotion/target', 'cnn/image/emotion/test')
 
-train_t = pool.forward(train_t, pool_size, pool_stride)
-train_t = pool.forward(train_t, pool_size, pool_stride)
-train_t = pool.forward(train_t, pool_size, pool_stride)
-test_t = pool.forward(test_t, pool_size, pool_stride)
-test_t = pool.forward(test_t, pool_size, pool_stride)
-test_t = pool.forward(test_t, pool_size, pool_stride)
+layer_count = 3
+
+for l in range(layer_count):
+    train_t = pool.forward(train_t, pool_size, pool_stride)
+    test_t = pool.forward(test_t, pool_size, pool_stride)
 
 print(train_t)
 
-c1_weight, c1_bias, c2_weight, c2_bias, c3_weight, c3_bias = train(train_x, train_t, convol_size, convol_stride, pool_size, pool_stride, learning_rate = 0.001, iterate = 5000)
+weight_list, bias_list = train(train_x, train_t, convol_size, convol_stride, pool_size, pool_stride, layer_count, learning_rate = 0.001, iterate = 5000)
 
-c1 = conv.forward(test_x, c1_weight, c1_bias, convol_stride)
-s1 = sigmoid.forward(c1)
-p1 = pool.forward(s1, pool_size, pool_stride)
+input = test_x
 
-c2 = conv.forward(p1, c2_weight, c2_bias, convol_stride)
-s2 = sigmoid.forward(c2)
-p2 = pool.forward(s2, pool_size, pool_stride)
+for l in range(layer_count):
+    c = conv.forward(input, weight_list[l], bias_list[l], convol_stride)
+    s = sigmoid.forward(c)
+    p = pool.forward(s, pool_size, pool_stride)
+    input = p
 
-c3 = conv.forward(p2, c3_weight, c3_bias, convol_stride)
-s3 = sigmoid.forward(c3)
-predict = pool.forward(s3, pool_size, pool_stride)
-
+predict = p
 
 predict = np.where(predict >= 0.5, 1.0, predict)
 predict = np.where(predict < 0.5, 0.0, predict)
