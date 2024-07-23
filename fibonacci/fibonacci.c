12b9587790499ffa56ff65f62688758d41cfdfa9
Ubuntu <ubuntu@ip-172-26-8-115.ap-northeast-2.compute.internal>
2024-07-23 01:11:37 +0000

    update2

--- fibonacci/fibonacci.c
+++ fibonacci/fibonacci.c
@@ -4,16 +4,16 @@
 int fibo(int n) ;
  
 int main(void){
-	int a = 1;
+	int a = 0;
 	int b = 1;
 	int c;
 
-	while(c < 34){
+	do{
 		c = a + b;
 		printf("%d\n" , c);
 		a = b;
 		b = c;
-	}
+	}while(c < 34);
 
 	return 0;
 									 
